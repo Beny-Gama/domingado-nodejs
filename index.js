87cf68bf29899e2b40706503284bc19ea3f2385d
Bernardo Gama <76955720+Beny-Gama@users.noreply.github.com>
2025-04-28 21:41:56 -0400

    CRUD aprendido

--- index.js
+++ index.js
@@ -3,33 +3,46 @@ const server = express();
 
 server.use(express.json());
 
+
 let customers = [
-    {id: 1, name: "dav Samirai", site: "http://devsamurai.com.br"},
-    {id: 2, name: "Google", site: "http://google.com"},
-    {id: 3, name: "UOL", site: "http://uol.com.br"},
-]
+    { id: 1, name: "Dev Samurai", site: "http://devsamurai.com.br" },
+    { id: 2, name: "Google", site: "http://google.com" },
+    { id: 3, name: "UOL", site: "http://uol.com.br" },
+];
 
-server.get("/customers", (req, res) => {
-    res.json(customers)
-})
+// Corrigido aqui: '/customers'
+server.get('/customers', (req, res) => {
+    return res.json(customers);
+});
 
-server.get('/customers/:id', (req, res) => {
-    const id = parseInt(req.params.id); //string
-    const customer = customers.find(item => item.id === id)
-    const status = customer ? 200 : 404;
-
-    return res.status(status).json(customer)
-})
-
-server.post("/customers", (req, res) => {
+server.put('/customers/:id', (req, res) => {
+    const id = parseInt(req.params.id);
     const { name, site } = req.body;
-    const id = customers[customers.length - 1 ].id + 1;
 
-    const newCustomer = { id, name, site };
-    customers.push(newCustomer);
+    const index = customers.findIndex(item => item.id === id);
+    const status = index >= 0 ? 200 : 404;
 
-    return res.status(201).json(newCustomer);
+    if ( index >= 0 ) {
+        customers[index] = { id: parseInt(id), name, site}
+    }
+    return res.status(status).json(customers[index])
+    
+});
 
-})
+server.delete('/customers/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = customers.findIndex(item => item.id === id);
+    const status = index >= 0 ? 200 : 404;
 
-server.listen(3000);
+    if (index >= 0) {
+        customers.splice(index, 1)
+    }
+    
+    return res.status(status).json()
+
+});
+
+const port = 3000
+server.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
