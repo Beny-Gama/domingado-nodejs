a121024fba4b18bd2d204eb9f998d4ea6c673803
Bernardo Gama <76955720+Beny-Gama@users.noreply.github.com>
2025-05-04 02:42:22 -0400

    train

--- index.js
+++ index.js
@@ -3,17 +3,27 @@ const server = express();
 
 server.use(express.json());
 
-
 let customers = [
     { id: 1, name: "Dev Samurai", site: "http://devsamurai.com.br" },
     { id: 2, name: "Google", site: "http://google.com" },
     { id: 3, name: "UOL", site: "http://uol.com.br" },
 ];
 
-// Corrigido aqui: '/customers'
 server.get('/customers', (req, res) => {
     return res.json(customers);
 });
+
+
+server.get('/customers/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const customer = customers.find(item => item.id === id);
+    const status = customer ? 200 : 404;
+
+    console.debug('GET :: /customers/:id', customer)
+
+    return res.status(status).json(customer);
+});
+
 
 server.put('/customers/:id', (req, res) => {
     const id = parseInt(req.params.id);
@@ -36,12 +46,10 @@ server.delete('/customers/:id', (req, res) => {
 
     if (index >= 0) {
         customers.splice(index, 1)
-    }
-    
+    };
     return res.status(status).json()
-
 });
-
+ 
 const port = 3000
 server.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
