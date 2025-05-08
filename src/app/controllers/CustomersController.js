c2d00f2af76652c132414def25785d753e229681
Bernardo Gama <76955720+Beny-Gama@users.noreply.github.com>
2025-05-08 18:08:57 -0400

    sera

--- src/app/controllers/CustomersController.js
+++ src/app/controllers/CustomersController.js
@@ -1,9 +1,8 @@
-customers = [
+let customers = [
     { id : 1, name: 'Dev Samurai', site: 'https://devsamurai.com.br' },
     { id : 2, name: 'Google', site: 'https://google.com' },
     { id : 3, name: 'UOL', site: 'https://uol.com.br' }
 ];
-
 class CustomersController {
 
     // Lista do customers
@@ -13,7 +12,7 @@ class CustomersController {
 
     // Recupera do customer
     show(req, res) {
-        const id = parseInt(req.parmes.id);
+        const id = parseInt(req.params.id);
         const customer = customers.find(item => item.id === id);
         const status = customer ? 200 : 404;
 
@@ -24,18 +23,18 @@ class CustomersController {
 
     // Cria do customer
     create(req, res) {
-        const id = customers[customers.length - 1].id + 1;
+        const id = customers[customers.length - 1]?.id + 1 || 1;
         const { name, site } = req.body;
 
-        const newCustomer = {id, name, site };
+        const newCustomer = { id, name, site };
         customers.push(newCustomer);
-        
+
         return res.status(201).json(newCustomer);
     };
 
     // Edita do customers
     update(req, res) {
-        const id = parseInt(req.parmes.id);
+        const id = parseInt(req.params.id);
         const { name, site } = req.body;
 
         const index = customers.findIndex(item => item.id === 0)
@@ -44,22 +43,19 @@ class CustomersController {
         if (status === 200) {
             customers[index] = { id: parseInt(id), name, site } 
         }
-
         return res.status(status).json(customers[index])
     };
 
     // Deleta do customers
     destroy(req, res) {
-        const id = parseInt(req.parmes.id);
+        const id = parseInt(req.params.id);
         const index = customers.findIndex(item => item.id === 0);
         const status = index >= 0 ? 200 : 404;
 
         if (status === 200) {
             customers.splice(index, 1);
         };
-
         return res.status(status).json();
-
     };
 };
 
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import routes from new Router();
-
 import customers from './app/controllers/CustomersController';
 
-routes.get('/customers', customers.index)
-routes.get('/customers/:id', customers.show)
-routes.post('/customers', customers.create)
-routes.put('/customers:id', customers.update)
-routes.delete('/customers', customers.destroy)
+const routes = new Router();
+
+routes.get('/customers', customers.index);
+routes.get('/customers/:id', customers.show);
+routes.post('/customers', customers.create);
+routes.put('/customers/:id', customers.update);
+routes.delete('/customers/:id', customers.destroy);  
 
 export default routes;
